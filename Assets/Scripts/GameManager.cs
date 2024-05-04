73fbddad470613a7cd1a6673154c8d9fc0a6bb5f
Nguyễn Quang Thắng <thangnq96tb@gmail.com>
2024-05-04 15:37:30 +0700

    *pick & update point

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,12 +17,18 @@ public class GameManager : MonoBehaviourSingleton<GameManager>
     int m_BoardCol, m_BoardRow;
     [SerializeField]
     GridLayoutGroup m_Board;
+    [SerializeField]
+    TextMeshProUGUI m_PickTxt;
 
     private float m_BoardWidth, m_BoardHeight;
     private int m_point = 0;
+    private int m_numberPick = 5;
+    private List<SCR_Item> m_listItem;
 
     private void Start()
     {
+        m_listItem = new List<SCR_Item>();
+
         m_BoardWidth = m_Board.GetComponent<RectTransform>().rect.width;
         m_BoardHeight = m_Board.GetComponent<RectTransform>().rect.height;
 
@@ -44,9 +51,11 @@ public class GameManager : MonoBehaviourSingleton<GameManager>
         }
 
         GenerateBoard();
+        UpdatePick();
     }
     public void GenerateBoard()
     {
+        m_listItem.Clear();
         foreach (Transform item in m_Board.transform)
         {
             Destroy(item.gameObject);
@@ -54,13 +63,37 @@ public class GameManager : MonoBehaviourSingleton<GameManager>
 
         for (int i = 0; i < m_BoardCol * m_BoardRow; i++)
         {
-            Instantiate(m_ItemPfb, m_Board.transform);
+            SCR_Item item = Instantiate(m_ItemPfb, m_Board.transform);
+            m_listItem.Add(item);
         }
+        m_numberPick++;
     }
 
     public void AddPoint(int amount)
     {
         m_point += amount;
         m_TotalPoint.text = m_point.ToString();
+    }    
+
+    public void UpdatePick()
+    {
+        m_numberPick--;
+        if(m_numberPick > 1)
+        {
+            m_PickTxt.text = $"{m_numberPick} picks!";
+        }
+        else if(m_numberPick == 1)
+        {
+            m_PickTxt.text = "1 pick!";
+        }
+        else
+        {
+            m_PickTxt.gameObject.SetActive(false);
+            m_listItem = m_listItem.Where(x => x.m_State == State.MYSTERY).ToList();
+            foreach(var item in m_listItem)
+            {
+                item.ShowItem();
+            }    
+        } 
     }    
 }
--- Assets/Scripts/SCR_Item.cs
+++ Assets/Scripts/SCR_Item.cs
@@ -4,16 +4,29 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum State
+{
+    MYSTERY,
+    SELECTED, 
+    UNSELECTED
+}
 public class SCR_Item : MonoBehaviour
 {
     [SerializeField]
     public Image m_ImgReward;
     [SerializeField]
     public TextMeshProUGUI m_Point;
+    [SerializeField]
+    public GameObject m_Selected;
+    [SerializeField]
+    public GameObject m_UnSelected;
+
+    public State m_State;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_State = State.MYSTERY;
     }
 
     // Update is called once per frame
@@ -25,15 +38,27 @@ public class SCR_Item : MonoBehaviour
     public void OnClick()
     {
         GameManager.Instance.AddPoint(ShowItem());
+        GameManager.Instance.UpdatePick();
+        m_State = State.SELECTED;
+        UpdateUI();
     }    
 
     public int ShowItem()
     {
+        m_State = State.UNSELECTED;
+        UpdateUI();
+        
         m_ImgReward.gameObject.SetActive(true);
         int point = Random.Range(1, 100);
         m_Point.text = point + "K";
         gameObject.GetComponent<Button>().interactable = false;
         return point;
+    }    
+
+    public void UpdateUI()
+    {
+        m_Selected.SetActive(m_State == State.SELECTED);
+        m_UnSelected.SetActive(m_State == State.UNSELECTED);
     }    
 
     public Vector2 GetSize()
